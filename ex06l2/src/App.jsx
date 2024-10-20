4176276ec93361bdaffeaf480a63d748fe42e34c
Anna Bertochi <anafbrtc@gmail.com>
2024-10-20 01:29:23 -0300

    Add files via upload

---
+++ ex06l2/src/App.jsx
@@ -0,0 +1,13 @@
+import Link from './components/Link'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+        <Link />
+    </>
+  );
+}
+
+export default App
---
+++ ex06l2/src/components/Link.jsx
@@ -0,0 +1,10 @@
+function Link() {
+    return (
+      <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+       <h1>Visite o site do ReactJS</h1> 
+      </a>
+    );
+  }
+  
+  export default Link;
+  
